0b3350b177fa8d95473e0d5e80e07f1076e5713a
Vidhur Vohra <dkzjunk@yahoo.com>
2012-03-28 23:35:13 -0400

    moving from laptop

--- src/MyAnt.java
+++ src/MyAnt.java
@@ -3,9 +3,18 @@ import java.util.Random;
 import ants.*;
 
 public class MyAnt implements Ant {
+	int homex, homey;
+	boolean savedHome = false;
 	Random rand = new Random();
 
 	public Action getAction(Surroundings surroundings) {
+		if (!savedHome){
+			;
+			//surroundings.getCurrentTile().
+		}
+			
+		if (surroundings.getCurrentTile().getAmountOfFood() > 0)
+			return Action.GATHER;
 		int rInt = rand.nextInt(4);
 		return Action.move(Direction.values()[rInt]);
 		// return Action.move(Direction.EAST);
