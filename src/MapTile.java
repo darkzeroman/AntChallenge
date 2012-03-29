f02b377eb921be7f907d76ae488e40fc567898a0
Vidhur Vohra <dkzjunk@yahoo.com>
2012-03-29 16:50:32 -0400

    inital work

---
+++ src/MapTile.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author dkz
+ * 
+ */
+public class MapTile {
+
+	MyAnt.type type;
+	int amountFood = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public MapTile(MyAnt.type tileType) {
+		this.type = tileType;
+	}
+
+}
--- src/MyAnt.java
+++ src/MyAnt.java
@@ -1,35 +1,70 @@
 import java.util.Random;
 
-import ants.*;
+import ants.Action;
+import ants.Ant;
+import ants.Direction;
+import ants.Surroundings;
 
 public class MyAnt implements Ant {
-	int homex, homey;
-	boolean savedHome = false;
+	public static enum type {
+		HOME, WALL, UNEXPLORED, FOOD
+	};
+	MapTile[][] map = new MapTile[40][40];
+	boolean initialized = false;
+
 	Random rand = new Random();
+	ObjectIO<Integer> oio = new ObjectIO<Integer>();
+	boolean carryingFood = false;
 
 	public Action getAction(Surroundings surroundings) {
-		if (!savedHome){
-			;
-			//surroundings.getCurrentTile().
-		}
-			
-		if (surroundings.getCurrentTile().getAmountOfFood() > 0)
-			return Action.GATHER;
-		int rInt = rand.nextInt(4);
-		return Action.move(Direction.values()[rInt]);
+		if (!initialized)
+
+			if (surroundings.getCurrentTile().getAmountOfFood() > 0
+					&& !carryingFood) {
+				carryingFood = true;
+				return Action.GATHER;
+			}
+		return Action.move(getRandomDirection(surroundings));
 		// return Action.move(Direction.EAST);
 	}
 
+	public void intializeMap(){
+		for (int i  =0; i < map.length; i++)
+			for (int j = 0; j < map[i].length; j++){
+				map[i][j] = new MapTile(type.HOME);
+			}
+	}
+	private Direction getRandomDirection(Surroundings surroundings) {
+		boolean[] choices = new boolean[4];
+		for (int i = 0; i < 4; i++) {
+			if (surroundings.getTile(Direction.values()[i]).isTravelable())
+				choices[i] = true;
+
+		}
+
+		int rInt = rand.nextInt(4);
+		if (choices[rInt])
+			return Direction.values()[rInt];
+		while (!choices[rInt])
+			rInt = rand.nextInt(4);
+		return Direction.values()[rInt];
+
+	}
+
 	public byte[] send() {
-		return null;
+		return oio.toByteArray(new Integer(5));
+		// return null;
 	}
 
 	public void receive(byte[] data) {
+		int x = oio.fromByteArray(data);
 		// Do nothing
+		System.out.println("RECEIVED X: " + x);
 	}
 
 	public static void main(String[] args) {
-
+		new MyAnt();
+		System.out.println("hi");
 	}
 
 }
