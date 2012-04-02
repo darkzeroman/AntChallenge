e93ab59398a73d87e60df5045676081d7531f5cb
Vidhur Vohra <dkzjunk@yahoo.com>
2012-04-01 20:33:07 -0400

    merging

--- src/MyAnt.java
+++ src/MyAnt.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -51,9 +50,9 @@ public class MyAnt implements Ant {
 
 	public Action getAction(Surroundings surroundings) {
 
-//		Scanner sc = new Scanner(System.in);
-//		while (!sc.nextLine().equals(""))
-//			;
+		// Scanner sc = new Scanner(System.in);
+		// while (!sc.nextLine().equals(""))
+		// ;
 
 		round++;
 		System.out.println();
